c7c1ffc971eb1267e6108c02bcebe9c78e7d668d
SANZU0007 <123228205+SANZU0007@users.noreply.github.com>
2023-02-02 13:38:40 +0530

    this my iniziele stage

---
+++ sanjaysanj.js
@@ -0,0 +1,48 @@
+let sanjay = {
+    "personal_info": {
+      "name": "John Doe",
+      "email": "johndoe@email.com",
+      "phone": "555-555-5555",
+      "location": "San Francisco, CA"
+    },
+    "education": [
+      {
+        "degree": "Bachelor of Science",
+        "field_of_study": "Computer Science",
+        "school_name": "University of California, Berkeley",
+        "graduation_date": "May 2020"
+      }
+    ],
+    "work_experience": [
+      {
+        "job_title": "Software Engineer",
+        "company_name": "Google",
+        "start_date": "June 2020",
+        "end_date": "Present",
+        "responsibilities": [
+          "Develop and maintain software applications",
+          "Collaborate with cross-functional teams to solve complex problems",
+          "Write clean, maintainable, and efficient code"
+        ]
+      },
+      {
+        "job_title": "Software Intern",
+        "company_name": "Microsoft",
+        "start_date": "May 2019",
+        "end_date": "August 2019",
+        "responsibilities": [
+          "Assisted senior software engineers with various tasks",
+          "Implemented new features for software applications",
+          "Provided technical support to customers"
+        ]
+      }
+    ],
+    "skills": [
+      "JavaScript",
+      "React",
+      "Node.js",
+      "Python",
+      "Agile methodologies"
+    ]
+  }
+  
