5fd8a512fed394a63c6f7eedde377bcda91a4a35
SANZU0007 <123228205+SANZU0007@users.noreply.github.com>
2023-07-08 11:41:02 +0530

    resume-json.js

--- sanjaysanj.js
+++ sanjaysanj.js
@@ -1,41 +1,24 @@
 let sanjay = {
     "personal_info": {
-      "name": "John Doe",
+      "name": "sanjay",
       "email": "johndoe@email.com",
       "phone": "555-555-5555",
-      "location": "San Francisco, CA"
+      "location": "india, tn"
     },
     "education": [
       {
         "degree": "Bachelor of Science",
         "field_of_study": "Computer Science",
-        "school_name": "University of California, Berkeley",
+        "school_name": "University of kamaraj, madurai",
         "graduation_date": "May 2020"
       }
     ],
     "work_experience": [
       {
-        "job_title": "Software Engineer",
-        "company_name": "Google",
-        "start_date": "June 2020",
-        "end_date": "Present",
-        "responsibilities": [
-          "Develop and maintain software applications",
-          "Collaborate with cross-functional teams to solve complex problems",
-          "Write clean, maintainable, and efficient code"
-        ]
-      },
-      {
-        "job_title": "Software Intern",
-        "company_name": "Microsoft",
-        "start_date": "May 2019",
-        "end_date": "August 2019",
-        "responsibilities": [
-          "Assisted senior software engineers with various tasks",
-          "Implemented new features for software applications",
-          "Provided technical support to customers"
-        ]
+        "fresher"
       }
+        ]
+    
     ],
     "skills": [
       "JavaScript",
